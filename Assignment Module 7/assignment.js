a191ff3b0aced836231749e004dce4d5a5e50fe7
Touhid <140738779+Kyotoka@users.noreply.github.com>
2025-05-16 22:45:28 +0600

    Update assignment.js

--- Assignment Module 7/assignment.js
+++ Assignment Module 7/assignment.js
@@ -50,3 +50,54 @@ console.log("The First Character Of The Given String Is :" + getNthChar("Got Fir
 
 // Extra 5
 
+// POSSITIVE OR NEGGETIVE
+
+function checkNumber(number) {
+    if (number > 0) {
+    return "Positive number";
+    } else if (number < 0) {
+    return "Negative number";
+    } else {
+    return "Zero";
+    }
+}
+
+console.log("Result : " + checkNumber(5));
+
+
+// MULTIPLY NUMBER
+function mlt(x, y) {
+    if (typeof x !== "number")
+        return "1st entry is not a number"
+    else if (typeof y !== "number")
+        return "2nd entry is not a number"
+    else return x * y;
+}
+console.log(mlt(10, "s"));
+console.log(mlt(12, 7));
+console.log(mlt("y", 7));
+
+// MIN TO SEC
+function mTs(m) {
+    let s;
+    s = m * 60;
+    return s;
+} 
+console.log(mTs(5) + "s");
+
+
+// CONCATE STRING
+function strCon(c1, c2) {
+    return c1 + c2;
+}
+
+console.log("like what", "you see ?")
+
+
+// CHECK IF THE SENTENCE HAS A CERTAIN WORD
+function checkStr(sentence, word) {
+
+return sentence.includes(word);
+}
+console.log(checkStr("Hi there", "boy"));
+console.log(checkStr("Hi there boy", "boy"));
